0e8f200a4dea5a30a826b2a447b048835b380331
roshan <roshan.krishnan@citysoft.net.au>
2019-06-23 21:07:43 +1000

    update title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'roshankrishnan7';
+  title = 'Roshan Krishnan Thirikkott | My Portfolio';
 }
