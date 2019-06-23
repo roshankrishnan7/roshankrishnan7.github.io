53d1a2eef1c08f7d81f94127d843ffaa0433ee9c
roshan <roshan.krishnan@citysoft.net.au>
2019-06-23 22:10:43 +1000

    update styling

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  title = 'Roshan Krishnan Thirikkott | My Portfolio';
+  title = 'Roshan Krishnan Thirikkott';
   constructor() { }
 
   ngOnInit() {
